b4b599b45c7fd5cc387fdc9c709c6349244c1512
kabil-jayaram <kabil.jayaram@gmail.com>
2024-03-07 19:22:32 +0530

    DoctorService Update
    DoctorService is now working

---
+++ DoctorService/src/main/java/com/example/service/AppointmentService.java
@@ -0,0 +1,51 @@
+package com.example.service;
+
+import com.example.entity.Appointment;
+import com.example.repository.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AppointmentService implements IAppointmentService{
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    @Override
+    public Appointment addAppointment(Appointment appointment) {
+        return appointmentRepository.save(appointment);
+    }
+
+    @Override
+    public Appointment getAppointmentById(int id) {
+        return appointmentRepository.findById(id).get();
+    }
+
+    @Override
+    public Appointment getAppointmentByPatientId(int patientId) {
+        return appointmentRepository.findAllByPatientId(patientId);
+    }
+
+    @Override
+    public List<Appointment> getAppointmentByDoctorId(int doctorId) {
+        return appointmentRepository.findAllByDoctorId(doctorId);
+    }
+
+    @Override
+    public List<Appointment> getAppointmentByStatus(String status) {
+        return appointmentRepository.findAllByStatus(status);
+    }
+
+    @Override
+    public Appointment updateAppointment(Appointment appointment) {
+        return appointmentRepository.save(appointment);
+    }
+
+    @Override
+    public Boolean deleteAppointment(int id) {
+        appointmentRepository.deleteById(id);
+        return true;
+    }
+}
