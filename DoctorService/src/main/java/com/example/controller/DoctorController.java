2fb8dff7996cbece12d10d974a72d3e42d8b1396
Tamilarasan1810 <92148875+Tamilarasan1810@users.noreply.github.com>
2024-03-07 14:24:13 +0530

    completed doctor controller

--- DoctorService/src/main/java/com/example/controller/DoctorController.java
+++ DoctorService/src/main/java/com/example/controller/DoctorController.java
@@ -1,6 +1,47 @@
 package com.example.controller;
 
+import com.example.entity.Doctor;
+import com.example.service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class DoctorController {
 
+    @Autowired
+    private DoctorService doctorService;
+
+    @GetMapping("/doctors")
+    public List<Doctor> getDoctors()
+    {
+        return doctorService.getAllDoctor();
+    }
+
+    @GetMapping("/doctor/{id}")
+    public Doctor getDoctorById(@PathVariable("id") int id)
+    {
+        return doctorService.getDoctoryById(id);
+    }
+
+    @GetMapping("/doctor")
+    public Doctor getDoctorBySpecialization(@RequestBody Doctor doctor)
+    {
+        return doctorService.getDoctorBySpecialization(doctor.getSpecialization());
+    }
+
+    @GetMapping("/doctor")
+    public Doctor getDoctorByIdAndSpecialization(@RequestBody Doctor doctor)
+    {
+        return doctorService.getDoctorByIdAndSpecialization(doctor.getId(), doctor.getSpecialization());
+    }
+    @DeleteMapping("/doctor/{id}")
+    public String deletelDoctor(@PathVariable("id") int id)
+    {
+        doctorService.deleteDoctor(id);
+        return "Doctor details deleted successfully !!!";
+    }
 
 }
