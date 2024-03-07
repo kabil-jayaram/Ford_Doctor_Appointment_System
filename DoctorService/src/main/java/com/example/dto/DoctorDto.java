5a1bcc1b85f2632ed11a2bd7602737930b5b5b1d
Tamilarasan1810 <92148875+Tamilarasan1810@users.noreply.github.com>
2024-03-07 15:13:06 +0530

    implemented Dto's

--- DoctorService/src/main/java/com/example/dto/DoctorDto.java
+++ DoctorService/src/main/java/com/example/dto/DoctorDto.java
@@ -1,4 +1,17 @@
 package DoctorService.src.main.java.com.example.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DoctorDto {
+    private int id;
+    private String name;
+    private String specialization;
+    private Map<Integer,Boolean> timeSlot;
 }
