81f4cf19d2cfd06db1d9366229c82d0c5915741a
Harshini S <65809+HS24@users.noreply.github.ford.com>
2024-03-08 10:32:05 +0530

    Patient Service

---
+++ PatientService/src/main/java/com/example/Dto/PatientDto.java
@@ -0,0 +1,15 @@
+package com.example.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PatientDto {
+    private int id;
+    private int name;
+    private int Age;
+    private String diagnosis;
+}
