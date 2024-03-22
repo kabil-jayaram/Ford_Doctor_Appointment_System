f036514bb6a52a45004a5ad372df25ef1e6f61dd
Tamilarasan1810 <92148875+Tamilarasan1810@users.noreply.github.com>
2024-03-22 17:49:21 +0530

    implemented doctor controller in security application

--- SpringSecurity/src/main/java/com/example/springsecurity/SpringSecurityApplication.java
+++ SpringSecurity/src/main/java/com/example/springsecurity/SpringSecurityApplication.java
@@ -2,10 +2,17 @@ package com.example.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
 
+        @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplateBuilder().build();
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityApplication.class, args);
     }
