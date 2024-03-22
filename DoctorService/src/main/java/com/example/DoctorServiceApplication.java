b79c9d0da91c89247cde65a6d1bdf6a7b1c9bead
kabil-jayaram <kabil.jayaram@gmail.com>
2024-03-22 17:25:25 +0530

    Revert "Doctor Testing Completed 🤌"
    This reverts commit 6b4e25c3b0aeffa0100bfc8937d7767386d18274.

--- DoctorService/src/main/java/com/example/DoctorServiceApplication.java
+++ DoctorService/src/main/java/com/example/DoctorServiceApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DoctorServiceApplication {
+	@Autowired
+	DoctorService doctorService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(com.example.DoctorServiceApplication.class, args);
